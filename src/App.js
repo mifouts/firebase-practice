d9ee9677a0a279214ff638a292dd759a5127618c
mifouts <fouts.mitch15@gmail.com>
2022-11-21 15:00:47 -0500

    hard registering a user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
 import { auth } from './firebase/init';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
 
 function App() {
   function register() {
-    
+    createUserWithEmailAndPassword(auth, 'email@email.com', 'password')
+    .then((user) => {
+      console.log(user)
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   }
   return (
     <div className="App">
